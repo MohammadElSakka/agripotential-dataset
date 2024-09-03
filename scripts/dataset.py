95953d7cf9f616eb8d35ab51a1b58fd2369fa298
Mohammad El Sakka <mohammad.el-sakka@irit.fr>
2024-09-03 14:10:50 +0200

    Add __get_paths__() to Dataset

--- scripts/dataset.py
+++ scripts/dataset.py
@@ -6,7 +6,7 @@ import shutil
 
 class Dataset:
     def __init__(self) -> None:
-        pass
+        self.data_path = {}
 
     def __download__(self) -> bool:
         if os.path.exists("data"):
@@ -28,3 +28,22 @@ class Dataset:
         else:
             print(f"Failed to download zip file [status code {response.status_code}].")
             return False
+        
+    def __get_paths__(self) -> None:
+        self.data_path["Sentinel-2"] = {}
+        years = os.listdir("data/sentinel2_bands")
+        for year in years:
+            y = int(year)
+            self.data_path["Sentinel-2"][y] = {}
+            months = os.listdir(f"data/sentinel2_bands/{year}")
+            for month in months:
+                m = int(month.split("_")[0])
+                self.data_path["Sentinel-2"][y][m] = {}
+                bands = os.listdir(f"data/sentinel2_bands/{year}/{month}/") 
+                for band in bands:
+                    band_name = band.split("_")[2]
+                    self.data_path["Sentinel-2"][y][m][band_name] = os.path.abspath(f"./data/sentinel2_bands/{year}/{month}/{band}")
+
+        self.data_path["labels"] = os.path.abspath("./data/dataset.geojson")
+        self.data_path["elevation"] = os.path.abspath("./data/elevation_data/raw_elevation_data_10m.tif")
+        self.data_path["weather"] = os.path.abspath("./data/weather_data/")
