f376677b807c9520a018e317db1e22fc194f00d9
Mohammad El Sakka <mohammad.el-sakka@irit.fr>
2024-09-04 10:27:48 +0200

    Update URL

--- scripts/dataset.py
+++ scripts/dataset.py
@@ -20,19 +20,18 @@ class Dataset:
 
     def __download__(self) -> bool:
         if os.path.exists("data"):
-            user_input = input("An existing data directory already exists. Override? (y/n)")
+            user_input = input("An existing data directory already exists. Override? (y/n) ")
             while user_input != "y" and user_input != "n":
-                user_input = input("An existing data directory already exists. Override? (y/n)")
+                user_input = input("An existing data directory already exists. Override? (y/n) ")
             if user_input == "y":
                 shutil.rmtree("data")
             else:
                 print(f"Failed to download zip file: data directory cannot be overrided.")
                 return False 
         
-        response = requests.get("https://cloud.irit.fr/s/cyt2sL9sASd4EJl/download")
+        response = requests.get("https://cloud.irit.fr/s/IVLWTCPV5AmNSZp/download")
         if response.status_code == 200:
             with zipfile.ZipFile(io.BytesIO(response.content)) as zip:
-                os.mkdir("data")
                 zip.extractall("data")
             return True
         else:
@@ -55,51 +54,10 @@ class Dataset:
                     self.data_path["sentinel2"][y][m][band_name] = os.path.abspath(f"./data/sentinel2_bands/{year}/{month}/{band}")
 
         self.data_path["labels"] = os.path.abspath("./data/dataset.geojson")
-        self.data_path["elevation"] = os.path.abspath("./data/elevation_data/raw_elevation_data_10m.tif")
+        self.data_path["elevation"] = os.path.abspath("./data/elevation_data/elevation_10m.tiff")
         self.data_path["weather"] = os.path.abspath("./data/weather_data/")
 
     def __get_sentinel2_meta__(self) -> None:
         self.sentinel2_meta = rasterio.open(self.data_path["sentinel2"][2019][1]['B02']).meta
 
-    # save to raw file
-    def get_altitude_data(self):
-
-        def get_mask(meta):
-            gdf = gpd.read_file( self.data_path["labels"])
-            gdf = gdf.to_crs(meta['crs'])
-            shape = meta['height'], meta['width']
-            transform = meta['transform']
-
-            labels = ["Limite", "Assez_limite", "Moyen", "Assez_fort", "Fort_a_tres_fort"]
-            mask = np.zeros((shape), dtype=bool)
-            for label in labels:
-                label_mask = geometry_mask(gdf[gdf["pot_global"] == label].geometry, 
-                                    out_shape=shape, 
-                                    transform=transform, 
-                                    invert=True)
-                mask |= label_mask
-            return mask 
-        # Resample altitude data to match the other bands
-        elevation_path = self.data_path["elevation"]
-        with rasterio.open(elevation_path) as src:
-            altitude_resampled = np.zeros((self.sentinel2_meta['height'], self.sentinel2_meta['width']), np.float32)
-
-            reproject(
-                source=rasterio.band(src, 1),
-                destination=altitude_resampled,
-                src_transform=src.transform,
-                src_crs=src.crs,
-                dst_transform=self.sentinel2_meta['transform'],
-                dst_crs=self.sentinel2_meta['crs'],
-                resampling=Resampling.nearest)
-        
-        mask = get_mask(self.sentinel2_meta)
-        altitude_resampled[altitude_resampled <-500] = 0
-        altitude_resampled[~mask] = np.nan 
-
-        with rasterio.open('elevation_10m.tiff', 'w', **self.sentinel2_meta) as dst:
-            dst.write(np.expand_dims(altitude_resampled, axis=0))  # Write the array, it automatically writes all bands
-        
-
-        return altitude_resampled
 
