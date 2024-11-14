7ddec99dcd960ac749963bfe4dd3cc56ab0d903b
Mohammad El Sakka <mohammad.el-sakka@irit.fr>
2024-11-14 11:58:58 +0100

    Add compute_boundaries

---
+++ scripts/utils.py
@@ -0,0 +1,23 @@
+def compute_boundaries(dataset):
+    binary_mask = dataset.get_binary_mask()
+    up, down, left, right = -1, -1, 1e8, -1
+    last_down = 0
+    
+    for i in range(binary_mask.shape[0]):
+        for j in range(binary_mask.shape[1]):
+            
+            if binary_mask[i][j] == 1 and up == -1:
+                up = i
+            
+            if binary_mask[i][j] == 1 and last_down == 0 and i != down:
+                down = i
+            
+            if binary_mask[i][j] == 1 and j<left:
+                left = j
+    
+            if binary_mask[i][j] == 1 and j>right:
+                right = j
+    
+            last_down = binary_mask[i][j]
+    # Will return 3321 10979 0 9401
+    return up, down, left, right
